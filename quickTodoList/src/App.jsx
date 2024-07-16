23d3664f8e37b3d613e3146bf959a6dd41412a44
brettl <lukas.brettl@seznam.cz>
2024-07-16 21:14:55 +0200

    long time I dont't programing so I started with something easy I build basic of easy To-do list

---
+++ quickTodoList/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import Task from "./Task.jsx"
+export default function App() {
+  
+  const [text, setText] = useState("")
+  const [tasks, setTasks] = useState([])
+  function addTask(){
+    if(text){
+      setTasks([...tasks, text])
+      setText("")
+    }
+
+  }
+  return (
+    <>
+    <nav className='p-4 pl-7'>
+      <h1 className='text-4xl font-semibold text-white'>To-do List</h1>
+    </nav>
+    <section className='flex flex-col justify-center items-center mt-16'>
+      <div className=' w-full h-12 flex justify-center items-center'>
+        <span className=' flex rounded-lg w-2/5 h-12 justify-between items-center' id="span_input">
+          <input className='w-4/5 text-white text-xl pl-4' id="input" value={text} onChange={(e) => setText(e.target.value)} placeholder='Write a task'></input>
+          <button id="add_button" onClick={() => addTask()} className='flex items-center w-auto p-1 rounded-lg font-medium h-auto text-lg text-white  mr-1'>add task</button>
+        </span>
+        <img className='w-8 h-8 ml-1 cursor-pointer' onClick={()=> setText("")} src="../public/icons8-trash-48.png"></img>
+      </div>
+      <div id="tasks" className='mt-24'>
+        <div id="list"> {/*the reason why I haven't used the ol list is because it doesn't work and I don't know why*/}
+          {tasks.map((task, index) => <Task key={index} text={task} num={index + 1}/>)}
+        </div>
+      </div>
+    </section>
+    <footer>
+
+    </footer>
+    </>
+  )
+}
---
+++ quickTodoList/src/Task.jsx
@@ -0,0 +1,57 @@
+import "./App.css"
+import "./App.jsx"
+import { useEffect, useState, useRef } from "react"
+export default function Task(props){
+    const [taskImportance, setTaskImportance] = useState("normal")
+    const [show, setShow] = useState(false)
+    const importance = {
+        "important": "#bf3032",
+        "unusual": "#ebb136",
+        "normal": "#b0b0b0",
+        "chill": "#96ba3c"
+
+    }
+    const buttonRef = useRef(null)
+    
+    useEffect(()=>{
+        if(buttonRef.current){
+            buttonRef.current.style.background = importance[taskImportance]
+        }
+        
+    },[taskImportance])
+
+    function findColor(){
+        return importance[taskImportance]
+    }
+
+    return(
+
+        <div className="flex mb-2 items-center justify-start">
+            <span className="text-lg font-semibold text-gray-400 mr-4 flex justify-center items-center">{props.num}</span>
+            <div id="task" className="text-white rounded-md h-12 text-xl pl-1.5 flex justify-start items-center w-80">
+                
+                <button ref={buttonRef} id="importance_button" onClick={() => (setShow(!show))} className="text-black font-semibold text-base rounded-3xl h-8 w-20 flex justify-center items-center p-1.5 mr-3" >{taskImportance}</button>
+                <ul id="importance_list" className=" shadow-lg rounded-lg  w-48 absolute" style={{"display": show ? "block": "none"}}>
+                    <li onClick={() => {setTaskImportance("important")
+                        setShow(!show)
+                    }} className="mb-3 flex items-center hover:bg-slate-600 w-full p-3 h-11 rounded-tr-lg rounded-tl-lg">important</li>
+                    <li onClick={() => {setTaskImportance("unusual")
+                        setShow(!show)
+                    }} className="mb-3 flex items-center hover:bg-slate-600 w-full p-3 h-11 ">unusual</li>
+                    <li onClick={() => {setTaskImportance("normal")
+                        setShow(!show)
+                    }} className="mb-3 flex items-center hover:bg-slate-600 w-full p-3 h-11 ">normal</li>
+                    <li onClick={() => {setTaskImportance("chill")
+                        setShow(!show)
+                    }} className=" flex items-center hover:bg-slate-600 w-full p-3 h-11 rounded-br-lg rounded-bl-lg">chill</li>
+                </ul>
+        
+                <span className="mr-2">
+                    {props.text}    
+                </span>   
+            </div>
+        </div>
+        
+        
+    )
+}
