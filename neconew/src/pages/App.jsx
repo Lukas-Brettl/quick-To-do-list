4f2121e8329d697db81d412b4d43fb4aff4fbc43
brettl <lukas.brettl@seznam.cz>
2024-09-16 21:21:29 +0200

    add Lesson.jsx and other things

--- neconew/src/pages/App.jsx
+++ neconew/src/pages/App.jsx
@@ -1,16 +1,27 @@
 import "../pagesCss/App.css"
 import Login from "./Login.jsx"
 import Mainapp from "./Mainapp.jsx"
+import Lesson from "./Lesson.jsx"
+import { useState } from "react"
 
 export default function App(){
+
+  const [data, setData] = useState(undefined)
+
+  function start(item, tema){
+    setData({item, tema})
+  
+  }
+
+  const logged = localStorage.getItem("jmeno") == null ||  localStorage.getItem("jmeno") == "" ?
+    <div id="main-login-div" className="flex justify-center items-center w-full h-full absolute">
+      <Login/>
+    </div>
+  : <Mainapp start={start}/> 
   return(
     <div>
       
-      {localStorage.getItem("jmeno") == null ||  localStorage.getItem("jmeno") == "" ?
-        <div id="main-login-div" className="flex justify-center items-center w-full h-full absolute">
-          <Login/>
-        </div>
-      : <Mainapp/> } 
+      {data? <Lesson item={data.item} tema={data.tema}/>: logged }
 
     </div>
   )
